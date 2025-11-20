736db984c5f20cab601c3a4042d626b0bf959346
Nevin <nevinxuregina@gmail.com>
2025-11-19 22:30:53 -0700

    worked on calendar scraper

--- calendar_scraper.py
+++ calendar_scraper.py
@@ -12,16 +12,54 @@ from selenium.common.exceptions import ElementNotInteractableException
 from selenium.common.exceptions import TimeoutException
 
 from webdriver_manager.chrome import ChromeDriverManager
+
+from bs4 import BeautifulSoup
 import time
 
+TIMEOUT = 1000000000000000000
+
 def login(driver):
+    load_dotenv()
+    username = os.getenv("username")
+    password = os.getenv("password")
+
+    url = "https://app.betterimpact.com/Login/admin"
+    driver.get(url)
+
+    # logging in
+    user_element = driver.find_element(By.CSS_SELECTOR, "#UserName")
+    password_element = driver.find_element(By.CSS_SELECTOR, "#Password")
+
+    user_element.send_keys(username)
+    password_element.send_keys(password)
+
+    login_button = driver.find_element(By.CSS_SELECTOR, "#SubmitLoginForm")
+    login_button.click()
+    pass
+
+def getVolunteers(driver):
+    WebDriverWait(driver, TIMEOUT).until(
+        expected_conditions.presence_of_element_located((By.CSS_SELECTOR, "a.favouriteLink"))
+    )
+    favLink = driver.find_element(By.CSS_SELECTOR, "a.favouriteLink")
+    favLink.click()
+
+    WebDriverWait(driver, TIMEOUT).until(
+        expected_conditions.presence_of_element_located((By.CSS_SELECTOR, "#ExpandAllShiftsButton"))
+    )
+    expandButton = driver.find_element(By.CSS_SELECTOR, "#ExpandAllShiftsButton")
+    expandButton.click()
+
+    soup = BeautifulSoup(driver.page_source)
+    print(soup)
+    time.sleep(100000)
     pass
 
 service = Service(ChromeDriverManager().install())
 op = webdriver.ChromeOptions()
-op.add_argument("--headless=new")
+#op.add_argument("--headless=new")
 driver = webdriver.Chrome(options = op, service = service)
-url = "https://app.betterimpact.com/Login/admin"
-driver.get(url)
-print(driver.page_source)
+driver.maximize_window()
+login(driver)
+getVolunteers(driver)
 time.sleep(10)
