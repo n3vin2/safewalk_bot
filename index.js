3555a37c8e38874d517beedc5da3f2ee1615a3da
Nevin Xu <nevinxuregina@gmail.com>
2025-11-23 23:54:43 -0700

    changed require() to import()

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ const spacings = [4, 5, 6, 6]
 
 let currentDay = new Date();
 
-const require = createRequire(import.meta.url);
+//const require = createRequire(import.meta.url);
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
@@ -29,31 +29,35 @@ const client = new Client({ intents: [GatewayIntentBits.Guilds] });
 
 client.commands = new Collection();
 
-const foldersPath = path.join(__dirname, "commands");
-const commandFolders = fs.readdirSync(foldersPath);
-for (const folder of commandFolders) {
-	const commandPath = path.join(foldersPath, folder);
-	const commandFiles = fs.readdirSync(commandPath).filter((file) => file.endsWith(".js"));
-	for (const file of commandFiles) {
-		const filePath = path.join(commandPath, file);
-		const command = require(filePath);
-		if ("data" in command && "execute" in command) {
-			client.commands.set(command.data.name, command);
-		} else {
-			console.log(`[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`);
+async function importCommands() {
+	const foldersPath = path.join(__dirname, "commands");
+	const commandFolders = fs.readdirSync(foldersPath);
+	for (const folder of commandFolders) {
+		const commandPath = path.join(foldersPath, folder);
+		const commandFiles = fs.readdirSync(commandPath).filter((file) => file.endsWith(".js"));
+		for (const file of commandFiles) {
+			const filePath = path.join(commandPath, file);
+			const command = await import(filePath);
+			if ("data" in command && "execute" in command) {
+				client.commands.set(command.data.name, command);
+			} else {
+				console.log(`[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`);
+			}
 		}
 	}
 }
 
-const eventsPath = path.join(__dirname, 'events');
-const eventFiles = fs.readdirSync(eventsPath).filter((file) => file.endsWith('.js'));
-for (const file of eventFiles) {
-	const filePath = path.join(eventsPath, file);
-	const event = require(filePath);
-	if (event.once) {
-		client.once(event.name, (...args) => event.execute(...args));
-	} else {
-		client.on(event.name, (...args) => event.execute(...args));
+async function importEvents() {
+	const eventsPath = path.join(__dirname, 'events');
+	const eventFiles = fs.readdirSync(eventsPath).filter((file) => file.endsWith('.js'));
+	for (const file of eventFiles) {
+		const filePath = path.join(eventsPath, file);
+		const event = await import(filePath);
+		if (event.once) {
+			client.once(event.name, (...args) => event.execute(...args));
+		} else {
+			client.on(event.name, (...args) => event.execute(...args));
+		}
 	}
 }
 
@@ -128,6 +132,9 @@ Tr = Trainer\n
 async function clientSetup() {
 	// Log in to Discord with your client's token
 	await client.login(token);
+
+	await importCommands();
+	await importEvents();
 
 	setInterval(async () => {
 		const now = new Date();
