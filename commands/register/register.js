d9e46380a9bcab06a67b07b7903e0d2901d5d680
Nevin <nevinxuregina@gmail.com>
2025-11-24 21:22:17 -0700

    removed ping command, added some authentication to the other commands

--- commands/register/register.js
+++ commands/register/register.js
@@ -1,24 +1,28 @@
-import { SlashCommandBuilder } from "discord.js";
+import { SlashCommandBuilder, PermissionsBitField } from "discord.js";
 import { readFile, writeFile } from "node:fs/promises";
 
 export const data = new SlashCommandBuilder().setName("register").setDescription("Set daily shift pings for this server.");
 
 export const execute = async (interaction) => {
     try {
-        const rawData = await readFile("registered_channels.json", "utf-8");
-        let data = JSON.parse(rawData);
-        if (!(interaction.guildId in data)) {
-            console.log('new guild');
-            data = {...data, [interaction.guildId]: {}};
+        if (interaction.user.id === process.env.dev_id || interaction.member.permissions.has(PermissionsBitField.Flags.Administrator)) {
+            const rawData = await readFile("registered_channels.json", "utf-8");
+            let data = JSON.parse(rawData);
+            if (!(interaction.guildId in data)) {
+                console.log('new guild');
+                data = {...data, [interaction.guildId]: {}};
+            }
+            if (interaction.channelId in data[interaction.guildId]) {
+                await interaction.reply("This channel has already been set for pings.");
+                return;
+            }
+            data[interaction.guildId] = {...data[interaction.guildId], [interaction.channelId]: null};
+            await writeFile("registered_channels.json", JSON.stringify(data));
+            await interaction.reply("This channel has been successfully set for pings!");
+
+        } else {
+            await interaction.reply("Insufficient permissions to run this command.");
         }
-        if (interaction.channelId in data[interaction.guildId]) {
-            await interaction.reply("This channel has already been set for pings.");
-            return;
-        }
-        data[interaction.guildId] = {...data[interaction.guildId], [interaction.channelId]: null};
-        await writeFile("registered_channels.json", JSON.stringify(data));
-        await interaction.reply("This channel has been successfully set for pings!");
-        
     } catch (exception) {
         console.log(exception);
         await interaction.reply("Something went wrong. Please try again.");
--- commands/register/unregister.js
+++ commands/register/unregister.js
@@ -1,19 +1,23 @@
-import { SlashCommandBuilder } from "discord.js";
+import { SlashCommandBuilder, PermissionsBitField } from "discord.js";
 import { readFile, writeFile } from "node:fs/promises";
 
 export const data = new SlashCommandBuilder().setName("unregister").setDescription("Remove daily shift pings for this server.");
 
 export const execute = async (interaction) => {
     try {
-        const rawData = await readFile("registered_channels.json", "utf-8");
-        let data = JSON.parse(rawData);
-        if (!(interaction.guildId in data) || !(interaction.channelId in data[interaction.guildId])) {
-            await interaction.reply("This channel was not set for pings.");
-            return;
+        if (interaction.user.id === process.env.dev_id || interaction.member.permissions.has(PermissionsBitField.Flags.Administrator)) {
+            const rawData = await readFile("registered_channels.json", "utf-8");
+            let data = JSON.parse(rawData);
+            if (!(interaction.guildId in data) || !(interaction.channelId in data[interaction.guildId])) {
+                await interaction.reply("This channel was not set for pings.");
+                return;
+            }
+            delete data[interaction.guildId][interaction.channelId]
+            await writeFile("registered_channels.json", JSON.stringify(data));
+            await interaction.reply("This channel will no longer be set for pings.");
+        } else {
+            await interaction.reply("Insufficient permissions to run this command.");
         }
-        delete data[interaction.guildId][interaction.channelId]
-        await writeFile("registered_channels.json", JSON.stringify(data));
-        await interaction.reply("This channel will no longer be set for pings.");
     } catch (exception) {
         console.log(exception);
         await interaction.reply("Something went wrong. Please try again.");
