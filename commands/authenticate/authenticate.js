0282e8262824b6b9667ea0e701300c75671a48d7
Nevin <nevinxuregina@gmail.com>
2026-01-08 03:30:26 -0700

    Finished the container for the authentication command

--- commands/authenticate/authenticate.js
+++ commands/authenticate/authenticate.js
@@ -1,18 +1,55 @@
-import { SlashCommandBuilder, ChatInputCommandInteraction } from "discord.js";
+import { SlashCommandBuilder, ChatInputCommandInteraction, ContainerBuilder, ButtonBuilder, ActionRowBuilder, MessageFlags, ButtonStyle } from "discord.js";
 
 export const data = new SlashCommandBuilder().setName("authenticate").setDescription("Authenticates discord user by linking them to their Better Impact email")
 
+const instructions = "### To link your Discord account to your BetterImpact E-mail, follow these instructions:\n\
+1. Click on the \"Enter E-mail\" button\n\
+2. Input the E-mail you used to sign up using BetterImpact\n\
+3. A code will be sent to that E-mail, copy that code\n\
+4. Click the \"Enter code\" button\n\
+5. Paste the code";
+
 export const execute = async (interaction) => {
-    let userId
+    let user
     if (interaction instanceof ChatInputCommandInteraction) {
-        userId = interaction.user.id;
+        user = interaction.user;
     } else {
-        userId = interaction.author.id;
+        user = interaction.author;
     }
     try {
-        
+        const container = new ContainerBuilder()
+            .setAccentColor(0xffff00)
+            .addTextDisplayComponents((textDisplay) => 
+                textDisplay.setContent("## Linking your Discord account to your BetterImpact E-mail")
+            )
+            .addTextDisplayComponents((textDisplay) => 
+                textDisplay.setContent(instructions)
+            )
+            .addActionRowComponents((actionRow) => 
+                actionRow.setComponents(
+                    new ButtonBuilder().setCustomId("email").setLabel("Enter E-mail").setStyle(ButtonStyle.Primary),
+                    new ButtonBuilder().setCustomId("code").setLabel("Enter code").setStyle(ButtonStyle.Secondary)
+                )
+            );
+        await user.send(
+            {
+                components: [container],
+                flags: MessageFlags.IsComponentsV2
+            }
+        );
+        await interaction.reply(
+            {
+                content: "I have sent the instructions in your DMs",
+                flags: MessageFlags.Ephemeral
+            }
+        );
     } catch (exception) {
         console.log(exception);
-        await interaction.reply("Something went wrong. Please try again.");
+        await interaction.reply(
+            {
+                content: "Something went wrong. Please try again.",
+                flags: MessageFlags.Ephemeral
+            }
+        );
     }
 }
