6b7c422a6fbfd6d0c28241507d013fdbf2e8592b
Nevin <nevinxuregina@gmail.com>
2026-03-04 22:43:04 -0700

    added some ORM models

---
+++ models/users.js
@@ -0,0 +1,38 @@
+'use strict';
+const {
+	Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+	class users extends Model {
+		/**
+		 * Helper method for defining associations.
+		 * This method is not a part of Sequelize lifecycle.
+		 * The `models/index` file will call this method automatically.
+		 */
+		static associate(models) {
+			// define association here
+		}
+	}
+	users.init({
+		UUID: {
+			type: DataTypes.UUID,
+			type: DataTypes.UUIDV4
+		},
+		Discord_ID: {
+			type: DataTypes.STRING,
+			allowNull: false
+		},
+		Email: {
+			type: DataTypes.STRING,
+			allowNull: false
+		},
+		Authenticated_At: {
+			type: DataTypes.DATE,
+			allowNull: false
+		}
+	}, {
+		sequelize,
+		modelName: 'users',
+	});
+	return users;
+};
