ff089296ca4dcbfbf08a126af2843e127ab93a3e
Nevin <nevinxuregina@gmail.com>
2026-03-20 14:26:57 -0600

    worked on email code authentcation, still a few rough edges

--- app/discord/modals/authentication/code.js
+++ app/discord/modals/authentication/code.js
@@ -1,13 +1,52 @@
-import { ModalBuilder, TextInputBuilder, TextInputStyle, LabelBuilder, MessageFlags, InteractionCollector } from "discord.js";
+import { MessageFlags } from "discord.js";
+import { prisma } from "../../../services/db.js";
+import crypto from "node:crypto";
 
 export const customId = "codeModal";
 
 export const execute = async (interaction) => {
-    console.log("codeInput");
-    console.log(interaction.fields.getTextInputValue())
+    await interaction.deferReply();
+    const code = interaction.fields.getTextInputValue("codeInput");
+
+    const auth_entry = await prisma.authcode.findUnique(
+        {
+            where: {
+                discord_id: interaction.user.id,
+                code_hash: crypto.createHash("sha256").update(code).digest("hex")
+            }
+        }
+    );
+
+    if (!auth_entry) {
+        await interaction.editReply(
+            {
+                content: "The code you entered is incorrect.",
+                flags: MessageFlags.Ephemeral
+            }
+        );
+        return;
+    }
+
+    await prisma.user.create(
+        {
+            data: {
+                discord_id: interaction.user.id,
+                email: auth_entry.email
+            }
+        }
+    );
+
+    await prisma.authcode.delete(
+        {
+            where: {
+                discord_id: interaction.user.id
+            }
+        }
+    );
+
     await interaction.editReply(
         {
-            content: "some stuff",
+            content: "Successfully linked your account to this email!",
             flags: MessageFlags.Ephemeral
         }
     );
