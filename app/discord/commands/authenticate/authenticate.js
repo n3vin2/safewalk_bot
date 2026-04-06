3428a1643d70847b7eddf890c5cd5b19a46c72a1
Nevin <nevinxuregina@gmail.com>
2026-04-06 02:10:36 -0600

    changed authenticate command to send menu, will not double reply in DMs

--- app/discord/commands/authenticate/authenticate.js
+++ app/discord/commands/authenticate/authenticate.js
@@ -1,11 +1,11 @@
-import { SlashCommandBuilder, ChatInputCommandInteraction, ContainerBuilder, ButtonBuilder, ActionRowBuilder, MessageFlags, ButtonStyle } from "discord.js";
+import { SlashCommandBuilder, ChatInputCommandInteraction, ContainerBuilder, ButtonBuilder, ChannelType, MessageFlags, ButtonStyle } from "discord.js";
 
 export const data = new SlashCommandBuilder().setName("authenticate").setDescription("Authenticates discord user by linking them to their Better Impact email")
 
-const instructions = "### To link your Discord account to your BetterImpact E-mail, follow these instructions:\n\
-1. Click on the \"Enter E-mail\" button\n\
-2. Input the E-mail you used to sign up using BetterImpact\n\
-3. A code will be sent to that E-mail, copy that code\n\
+const instructions = "### To link your Discord account to your Safewalk email, follow these instructions:\n\
+1. Click on the \"Enter Email\" button\n\
+2. Input the E-mail you used to sign up for Safewalk\n\
+3. A code will be sent to that Email, copy that code\n\
 4. Click the \"Enter code\" button\n\
 5. Paste the code";
 
@@ -19,24 +19,34 @@ export const execute = async (interaction) => {
     try {
         const container = new ContainerBuilder()
             .setAccentColor(0xffff00)
-            .addTextDisplayComponents((textDisplay) => 
-                textDisplay.setContent("## Linking your Discord account to your BetterImpact E-mail")
+            .addTextDisplayComponents(
+                textDisplay => textDisplay.setContent("# Safewalk Email Linking Menu"),
+                textDisplay => textDisplay.setContent("## Linking your Discord account to your Safewalk email")
             )
             .addTextDisplayComponents((textDisplay) => 
                 textDisplay.setContent(instructions)
             )
             .addActionRowComponents((actionRow) => 
                 actionRow.setComponents(
-                    new ButtonBuilder().setCustomId("email").setLabel("Enter E-mail").setStyle(ButtonStyle.Primary),
-                    new ButtonBuilder().setCustomId("code").setLabel("Enter code").setStyle(ButtonStyle.Secondary)
+                    new ButtonBuilder().setCustomId("email").setLabel("Enter Email").setStyle(ButtonStyle.Primary),
+                    new ButtonBuilder().setCustomId("code").setLabel("Enter Code").setStyle(ButtonStyle.Secondary),
+                    new ButtonBuilder().setCustomId("unlink").setLabel("Unlink Email").setStyle(ButtonStyle.Danger)
                 )
             );
+
+        if (interaction.channel.type === ChannelType.DM) {
+            await interaction.reply({
+                components: [container],
+                flags: MessageFlags.IsComponentsV2
+            });
+            return;
+        }
         await user.send({
             components: [container],
             flags: MessageFlags.IsComponentsV2
         });
         await interaction.reply({
-            content: "I have sent the instructions in your DMs",
+            content: "I have sent the menu in your DMs",
             flags: MessageFlags.Ephemeral
         });
     } catch (exception) {
