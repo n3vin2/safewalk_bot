84e4d3d24d896797f5bac4f1a84cdbc2f048082b
Nevin <nevinxuregina@gmail.com>
2026-04-06 02:13:48 -0600

    fixed spacing, used upsert to reduce code, added checking in case user has already been linked to an email

--- app/discord/modals/authentication/code.js
+++ app/discord/modals/authentication/code.js
@@ -6,6 +6,19 @@ export const customId = "codeModal";
 
 export const execute = async (interaction) => {
     await interaction.deferReply();
+
+    const discord_user = await prisma.user.findUnique({
+        where: { discord_id: interaction.user.id }
+    });
+
+    if (discord_user) {
+        await interaction.editReply({
+            content: "Your Discord account is already linked to an email. You must unlink first.",
+            flags: MessageFlags.Ephemeral
+        });
+        return;
+    }
+
     const code = interaction.fields.getTextInputValue("codeInput");
 
     const auth_entry = await prisma.authcode.findUnique({
